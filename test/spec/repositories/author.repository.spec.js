8e0eb9f589efc430643d65c62a95d61c013637b5
Donald Jones <jones.donald@gmail.com>
2015-07-07 23:17:24 -0500

    Added spec for author repo.

---
+++ test/spec/repositories/author.repository.spec.js
@@ -0,0 +1,11 @@
+var chai = require('chai'),
+	sinon = require('sinon'),
+	sinonChai = require('sinon-chai'),
+	expect = chai.expect,
+	AuthorRepo = require('../../../repositories/author/author.repository');
+
+chai.use(sinonChai);
+
+describe('Author Repository', function () {
+	
+});
